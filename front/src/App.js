f7e64b403ee829544f23611583e2d11248bd6a00
Romain Roux <cuisinier.sonore@gmail.com>
2019-04-18 22:43:18 +0200

    jeux eggs en cours

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-// import FinalGame from './Component/FinalGame';
+import FinalGame from './Component/FinalGame';
 import './CSS/App.css';
-import History from './Component/History';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>Escape Eggs</h1>
-      {/* <FinalGame /> */}
-        <History />
+      <FinalGame />
       </div>
     );
   }
