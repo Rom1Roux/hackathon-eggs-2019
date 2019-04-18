feb3f215411edaea8abcecb9a31064b0e3ba5a25
Romain Roux <cuisinier.sonore@gmail.com>
2019-04-19 00:46:31 +0200

    voice OK

--- front/src/Component/History.jsx
+++ front/src/Component/History.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import ChoiceScenario from './ChoiceScenario';
+import Say from 'react-say';
 
 export default class History extends Component {
   constructor(props) {
@@ -12,9 +13,8 @@ export default class History extends Component {
   }
 
 
-  // componentDidMount() {
-  //   this.scenario();
-  // }
+  componentDidMount() {
+  }
 
   next = (param) => {
     const next = this.state.format.answerChoice[param].nextFunction;
@@ -25,11 +25,13 @@ export default class History extends Component {
     const { histoire, answerChoice } = this.state.format;
     return (
       <div>
+
         <p>{histoire}</p>
+        <Say speak={histoire} />
         <Button.Group vertical>
-        <div className='buttonChoice'>
-          <Button onClick={() => this.next('one')} color='red'>{answerChoice.one.text}</Button>
-        </div>
+          <div className='buttonChoice'>
+            <Button onClick={() => this.next('one')} color='red'>{answerChoice.one.text}</Button>
+          </div>
           <div className='buttonChoice'>
             <Button onClick={() => this.next('two')} color='red'>{answerChoice.two.text}</Button>
           </div>
--- front/src/Component/Start.jsx
+++ front/src/Component/Start.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
-import History from './History'
+import History from './History';
+import Say from 'react-say';
 
 export default class Start extends Component {
 
@@ -17,6 +18,7 @@ export default class Start extends Component {
       <div>
         {this.state.start !== true ?
           <div>
+             <Say speak="GROS PAVÉ DE TEXTE QUI EXPLIQUE L'HISTOIRE" />
             <p>GROS PAVÉ DE TEXTE QUI EXPLIQUE L'HISTOIRE</p>
             <Button className='buttonChoice' onClick={this.startHistory}>GOOO !</Button>
           </div>
