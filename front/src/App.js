f11aeac1b747e1f08d2ff7ef04ae8a69d1ceb8db
Romain Roux <cuisinier.sonore@gmail.com>
2019-04-18 11:47:40 +0200

    git config

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>Escape Eggs</h1>
         
       </div>
     );
